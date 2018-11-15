d2cce2e6b7715241c8013f6227fa4502a0e2d8a1
Artem Kharytoniuk <artemalive@gmail.com>
2018-11-15 23:43:22 +0100

    More cleanup.

--- src/shaders/rt_utils.glsl
+++ src/shaders/rt_utils.glsl
@@ -21,22 +21,20 @@ struct Ray {
 vec3 get_direction(vec2 film_position) {
     const float tan_fovy_over_2 = 0.414; // tan(45/2)
 
-    vec2 uv = film_position / vec2(gl_LaunchSizeNVX.xy);
+    vec2 uv = 2.0 * (film_position / vec2(gl_LaunchSizeNVX.xy)) - 1.0;
     float aspect_ratio = float(gl_LaunchSizeNVX.x) / float(gl_LaunchSizeNVX.y);
-    float horz_half_dist = aspect_ratio * tan_fovy_over_2;
-    float vert_half_dist = tan_fovy_over_2;
-    vec2 uv2 = 2.0 * uv - 1.0;
-    float dir_x = uv2.x * horz_half_dist;
-    float dir_y = -uv2.y * vert_half_dist;
+
+    float dir_x =  uv.x *  aspect_ratio * tan_fovy_over_2;
+    float dir_y = -uv.y * tan_fovy_over_2;
     return normalize(vec3(dir_x, dir_y, -1.f));
 }
 
 Ray generate_ray(mat4x3 camera_to_world, vec2 film_position) {
     Ray ray;
-    ray.origin = camera_to_world[3];
-    ray.dir = camera_to_world * vec4(get_direction(film_position), 0);
-    ray.rx_dir = camera_to_world * vec4(get_direction(vec2(film_position.x + 1.f, film_position.y)), 0);
-    ray.ry_dir = camera_to_world * vec4(get_direction(vec2(film_position.x, film_position.y + 1.f)), 0);
+    ray.origin  = camera_to_world[3];
+    ray.dir     = camera_to_world * vec4(get_direction(film_position), 0);
+    ray.rx_dir  = camera_to_world * vec4(get_direction(vec2(film_position.x + 1.f, film_position.y)), 0);
+    ray.ry_dir  = camera_to_world * vec4(get_direction(vec2(film_position.x, film_position.y + 1.f)), 0);
     return ray;
 }
 #endif // RGEN_SHADER
