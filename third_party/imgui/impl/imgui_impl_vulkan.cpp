50099105d65bddf9974a2a2b5d30935bb3a7b6f4
Artem Kharytoniuk <artemalive@gmail.com>
2022-01-02 21:43:10 +0100

    ImGui: Make ImGui work with volk loader
    ImGui defines vulkan function pointers globally and those names
    conflict with function pointers defined by volk library (violation of
    single definition rule).
    
    This leads to runtime crash when some compilation units have a
    chance to use imgui's version of function pointer which is not
    initialized by imgui (because we use volk for initialization and do
    not invoke imgui function loader functionality).

--- third_party/imgui/impl/imgui_impl_vulkan.cpp
+++ third_party/imgui/impl/imgui_impl_vulkan.cpp
@@ -123,12 +123,12 @@ void ImGui_ImplVulkanH_CreateWindowCommandBuffers(VkPhysicalDevice physical_devi
 
 // Vulkan prototypes for use with custom loaders
 // (see description of IMGUI_IMPL_VULKAN_NO_PROTOTYPES in imgui_impl_vulkan.h
-#ifdef VK_NO_PROTOTYPES
+#if defined(VK_NO_PROTOTYPES) && !defined(VOLK_HEADER_VERSION)
 static bool g_FunctionsLoaded = false;
 #else
 static bool g_FunctionsLoaded = true;
 #endif
-#ifdef VK_NO_PROTOTYPES
+#if defined(VK_NO_PROTOTYPES) && !defined(VOLK_HEADER_VERSION)
 #define IMGUI_VULKAN_FUNC_MAP(IMGUI_VULKAN_FUNC_MAP_MACRO) \
     IMGUI_VULKAN_FUNC_MAP_MACRO(vkAllocateCommandBuffers) \
     IMGUI_VULKAN_FUNC_MAP_MACRO(vkAllocateDescriptorSets) \
@@ -1011,7 +1011,7 @@ bool    ImGui_ImplVulkan_LoadFunctions(PFN_vkVoidFunction(*loader_func)(const ch
     // You can use the default Vulkan loader using:
     //      ImGui_ImplVulkan_LoadFunctions([](const char* function_name, void*) { return vkGetInstanceProcAddr(your_vk_isntance, function_name); });
     // But this would be equivalent to not setting VK_NO_PROTOTYPES.
-#ifdef VK_NO_PROTOTYPES
+#if defined(VK_NO_PROTOTYPES) && !defined(VOLK_HEADER_VERSION)
 #define IMGUI_VULKAN_FUNC_LOAD(func) \
     func = reinterpret_cast<decltype(func)>(loader_func(#func, user_data)); \
     if (func == NULL)   \
